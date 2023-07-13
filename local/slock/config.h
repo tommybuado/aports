186b4035894ae70700b3a55e10b987a6ff3c91ea
Tommy Buado <tommy@thinkpad.local>
2023-07-14 03:27:41 +0800

    local/slock: update colorscheme and set monitortime and controlkeyclear

--- local/slock/config.h
+++ local/slock/config.h
@@ -3,16 +3,16 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#101010",   /* after initialization */
+	[INPUT] =  "#5f5fee",   /* during input */
+	[FAILED] = "#ff0090",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
 /* time in seconds before the monitor shuts down */
-static const int monitortime = 5;
+static const int monitortime = 300;
 
 /* allow control key to trigger fail on clear */
-static const int controlkeyclear = 0;
+static const int controlkeyclear = 1;
