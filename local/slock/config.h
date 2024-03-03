b595bc8fcfbd06eceef4aacfc9d99f2f659fcf6f
Tommy Buado <tommy@thinkpad.local>
2024-03-03 11:05:57 +0800

    local/slock: add configuration file

---
+++ local/slock/config.h
@@ -0,0 +1,18 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "#101010",   /* after initialization */
+	[INPUT] =  "#5f5fee",   /* during input */
+	[FAILED] = "#ff0090",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 300;
+
+/* set blur radius */
+static const int blurradius = 5;
